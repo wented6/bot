d8c617c659d04ee67955074e0754e72e2c8e9f1b
nekoboy7409 <38514326+nekoboy7409@users.noreply.github.com>
2018-05-21 16:34:38 -0400

    Update serverinfo.js

--- commands/serverinfo.js
+++ commands/serverinfo.js
@@ -8,13 +8,15 @@ let bots = message.guild.members.filter(mem => mem.user.bot === true).size;
 let txt = message.guild.channels.filter(c => c.type === 'text').size;
 let vcs = message.guild.channels.filter(c => c.type === 'voice').size;
 let cats = message.guild.channels.filter(c => c.type === 'category').size;
+let onr = message.guild.owner;
 
+if(onr){
 const RichEmbed = new Discord.RichEmbed()
       .setColor(`${message.member.displayHexColor}`)
        .setTimestamp()
 	   .setThumbnail(message.guild.iconURL)
 	   .setTitle(`${message.guild}`)
-	   .addField('**Owner:**', `${message.guild.owner.displayName}`)
+	   .addField('**Owner:**', `${onr.displayName}`)
        .addField('**Region:**', `${message.guild.region}`)
        .addField('**# of Roles:**', `${message.guild.roles.size}`)
 	   .addField('**# of Members:**', `${ppl} members, ${bots} bots`)
@@ -23,6 +25,23 @@ const RichEmbed = new Discord.RichEmbed()
 	   .addField('**(cats/txt/vc):**', `${cats}/ ${txt}/ ${vcs}`)
 	   message.channel.send({embed: RichEmbed})
 	   console.log(`serverinfo command has been used by ${message.author.username} in ${message.guild}`);
+}
+if(!onr){
+const RichEmbed = new Discord.RichEmbed()
+      .setColor(`${message.member.displayHexColor}`)
+       .setTimestamp()
+	   .setThumbnail(message.guild.iconURL)
+	   .setTitle(`${message.guild}`)
+       .addField('**Region:**', `${message.guild.region}`)
+       .addField('**# of Roles:**', `${message.guild.roles.size}`)
+	   .addField('**# of Members:**', `${ppl} members, ${bots} bots`)
+	   .addField('**# of Emojis:**', `${message.guild.emojis.size}`)
+       .addField('**Server Created:**', `${message.guild.createdAt}`)
+	   .addField('**(cats/txt/vc):**', `${cats}/ ${txt}/ ${vcs}`)
+	   message.channel.send({embed: RichEmbed})
+	   console.log(`serverinfo command has been used by ${message.author.username} in ${message.guild}`);
+}
+
 }
 module.exports.help = {
     name: "serverinfo"
