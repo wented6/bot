e01d10436d42a1dbc4336ef6fb4652423650a1ec
nekoboy7409 <38514326+nekoboy7409@users.noreply.github.com>
2018-05-22 02:07:20 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -217,7 +217,7 @@ client.on('message', async message => {
 						});
 					} catch (err) {
 						console.error(err);
-						return message.channel.send('No or invalid value entered, cancelling video selection.')then(msg=>{msg.delete(10000)});
+						return message.channel.send('No or invalid value entered, cancelling video selection.').then(msg=>{msg.delete(10000)});
 					}
 					const videoIndex = parseInt(response.first().content);
 					var video = await youtube.getVideoByID(videos[videoIndex - 1].id);
