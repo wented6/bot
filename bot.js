b19b7ed6672af30fbd9da524914b2eec0bae6ec9
nekoboy7409 <38514326+nekoboy7409@users.noreply.github.com>
2018-05-22 19:00:20 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -262,7 +262,7 @@ client.on('message', async message => {
           let embed = new Discord.RichEmbed()
           .setColor(`${message.member.displayHexColor}`)
           .addField('**Now Playing:**', `${serverQueue.songs[0].title}`)
-          .addField('**Time:**', `Elapsed: ${serverQueue.dispatcher.totalStreamTime} / total: ${serverQueue.songs[0].duration}`)
+          .addField('**Time:**', `Elapsed: ${serverQueue.connection.dispatcher.totalStreamTime} / total: ${serverQueue.songs[0].duration}`)
 		message.channel.send({embed}).then(msg=>{msg.delete(15000)});
 		message.delete(10000);
 	} else if (command === `queue`) {
