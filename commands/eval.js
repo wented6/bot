5720471e9af4ec18815cfc361aeef75f8813e5e2
nekoboy7409 <38514326+nekoboy7409@users.noreply.github.com>
2018-05-23 01:22:18 -0400

    Update eval.js

--- commands/eval.js
+++ commands/eval.js
@@ -1,4 +1,16 @@
 const Discord = require("discord.js");
+const parseTime = function(milliseconds) {
+  var seconds = Math.floor(milliseconds/1000); milliseconds %= 1000;
+  var minutes = Math.floor(seconds/60); seconds %= 60;
+  var hours = Math.floor(minutes/60); minutes %= 60;
+  var days = Math.floor(hours/24); hours %= 24;
+  var written = false;
+  return(days?(written=true,days+" days"):"")+(written?", ":"")
+      +(hours?(written=true,hours+" hours"):"")+(written?", ":"")
+      +(minutes?(written=true,minutes+" minutes"):"")+(written?", ":"")
+      +(seconds?(written=true,seconds+" seconds"):"")+(written?", ":"")
+      +(milliseconds?milliseconds+" milliseconds":"");
+};
 
 module.exports.run = async (client,message,args) => {
 
