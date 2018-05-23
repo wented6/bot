3ab49e7354a989403a6e996cc6a28557bf1a0e6d
nekoboy7409 <38514326+nekoboy7409@users.noreply.github.com>
2018-05-23 04:53:02 -0400

    Update invite.js

--- commands/invite.js
+++ commands/invite.js
@@ -6,11 +6,13 @@ module.exports.run = async (client,message,args) => {
 	.setColor(`${message.member.displayHexColor}`)
 	.setTimestamp()
 	.setThumbnail(`${client.user.avatarURL}`)
-	.setTitle(`${client.user.username}'s invite link`)
-	.addField(`**here is the link to invite me to a server:**`,`[**Nekobot™/invite**](https://discordapp.com/oauth2/authorize?client_id=${client.user.id}&scope=bot&permissions=8)`)
-	message.channel.send({embed: embed}).then(message => {message.delete(25000)});
-console.log(`sent invite link to ${message.author.username} in ${message.guild}`);
-	message.delete(10000);
+	.setTitle(`links:`)
+	.addField("**link to invite me to a server:**",`[Nekobot™/invite](https://discordapp.com/oauth2/authorize?client_id=${client.user.id}&scope=bot&permissions=8)`)
+	.addField("**link to my support sever:**", `[Nekobot™ official](https://discord.gg/SQFbf9q)`)
+	.addField("**if you like the bot, show it:**", `[upvote](https://discordbots.org/bot/389890733576028161/vote)`)
+	message.channel.send({embed: embed}).then(message => {message.delete(21000)});
+console.log(`sent links to ${message.author.username} in ${message.guild}`);
+	message.delete(20000);
 }
 
 module.exports.help = {
