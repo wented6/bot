ecb31a83b8daaab917fb6adb43cecc4b0d083f2a
nekoboy7409 <38514326+nekoboy7409@users.noreply.github.com>
2018-06-06 04:49:17 -0400

    Update botinfo.js

--- commands/botinfo.js
+++ commands/botinfo.js
@@ -1,47 +1,31 @@
 const Discord = require("discord.js");
-
+const parseTime = function(milliseconds) {
+    var seconds = Math.floor(milliseconds/1000); milliseconds %= 1000;
+    var minutes = Math.floor(seconds/60); seconds %= 60;
+    var hours = Math.floor(minutes/60); minutes %= 60;
+    var days = Math.floor(hours/24); hours %= 24;
+    var written = false;
+    return(days?(written=true,days+" days"):"")+(written?", ":"")
+      +(hours?(written=true,hours+" hours"):"")+(written?", ":"")
+      +(minutes?(written=true,minutes+" minutes"):"")+(written?", ":"")
+      +(seconds?(written=true,seconds+" seconds"):"")+(written?" ":"");
+};
 module.exports.run = async (client,message,args) => {
-    var neko = message.guild.members.find("id", "377271843502948354");
-	var now = Date.now();
-	var msec = now - client.readyTimestamp;
-	var days = Math.floor(msec / 1000 / 60 / 60 / 24);
-	msec -= days * 1000 * 60 * 60 * 24;
-	var hours = Math.floor(msec / 1000 / 60 / 60);
-	msec -= hours * 1000 * 60 * 60;
-	var mins = Math.floor(msec / 1000 / 60);
-	msec -= mins * 1000 * 60;
-	var secs = Math.floor(msec / 1000);
-	var timestr = "";
-	if(days > 0) {
-		timestr += days + " days ";
-	}
-	if(hours > 0) {
-		timestr += hours + " hours ";
-	}
-	if(mins > 0) {
-		timestr += mins + " minutes ";
-	}
-	if(secs > 0) {
-		timestr += secs + " seconds ";
-	}
-	console.log(`Current uptime is: ${timestr}`)
+    var neko = client.users.find("id", "377271843502948354");
 	const RichEmbed = new Discord.RichEmbed()
 	   .setColor(`${message.member.displayHexColor}`)
 	   .setFooter(`Requested by: ${message.member.displayName}`, `${message.author.avatarURL}`)
 		  .setTimestamp()
 		  .setThumbnail(client.user.avatarURL)
 		  .setTitle(`info for ${client.user.username}`)
-		  .addField('**My Owner:**', `${neko.user.tag}`)
+		  .addField('**Creator:**', `${neko.username}#${neko.discriminator}`)
 		  .addField('**Discord.js Version:**', `v${Discord.version}`)
 		  .addField('**Node.js Version:**', `${process.version}`)
 		  .addField('**Memory useage:**', `${(process.memoryUsage().heapUsed / 1024 / 1024).toFixed(2)} MB`)
 		  .addField('**# of servers im in:**', `${message.client.guilds.size}`)
 		  .addField('**# of users i have:**', `${client.users.size}`)
-		  .addField('**uptime:**', "" + timestr)
-		  .addField('**status:**', `${message.client.user.presence.status}`)
-		  .addField('**Playing:**', `${message.client.user.presence.game.name}`)
-		  .addField('**Created at:**', `${client.user.createdAt}`)
-		  .setDescription(`this is the bot's info`)
+		  .addField('**uptime:**', `${parseTime(client.uptime)}`)
+		  .addField('**Created on:**', '12/11/2017')
 	   message.channel.send({embed: RichEmbed})
 	   console.log(`botinfo command has been used by ${message.author.username} in ${message.channel.guild}`);
 }
