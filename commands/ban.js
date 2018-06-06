59e477b4f9063e60b54d671b22de538ab91463a6
nekoboy7409 <38514326+nekoboy7409@users.noreply.github.com>
2018-06-06 05:10:43 -0400

    Update ban.js

--- commands/ban.js
+++ commands/ban.js
@@ -1,20 +1,17 @@
 const Discord = require("discord.js");
 const fs = require("fs");
 let bans = require("../jsons/bans.json");
-let kicks = require("../jsons/kicks.json");
 
 module.exports.run = async (client,message,args) => {
-	let reason = args.join(" ").slice(22);
-  
-	message.delete(5000);
-	  
+let ugu = message.content.split(" ").slice(1).join(" ");	  
 let wUser = message.mentions.users.first();
-let wmem = message.mentions.members.first();
+let wmem = message.mentions.members.first()||message.guid.members.get("id", ugu);
 	  
 const neko = message.guild.members.find("id", "377271843502948354");
   if(!message.member.hasPermission("BAN_MEMBERS") && !neko)return message.reply('you have to have the `"BAN_MEMBERS"` permission to use this command');
-  if(!wUser) return message.reply("I couldn't find that member");
+  if(!wmem)return message.reply("I couldn't find that member");
   if (wmem === neko)return message.reply("i won't ban my master");
+  let reason = message.content.split(" ").slice(2).join(" ");
 
   if(!bans[message.author.id]) {
     bans[message.author.id] = {
@@ -39,30 +36,14 @@ const neko = message.guild.members.find("id", "377271843502948354");
 
 
 if (reason) {
-let warnbed = new Discord.RichEmbed()
-.setTitle(`You've been banned`)
-.setTimestamp()
-.setDescription(`from the server ${message.guild}`)
-.setColor(0xff0000)
-.addField('**By:**', `${message.member.displayName}`)
-.addField('**For:**', `${reason}`)
-wUser.send(warnbed);
 wmem.ban(reason);
 }
 if (!reason) {
-let warnbe = new Discord.RichEmbed()
-.setTitle(`You've been banned`)
-.setTimestamp()
-.setDescription(`in the server ${message.guild}`)
-.setColor(0xff0000)
-.addField('**By:**', `${message.member.displayName}`)
-wUser.send(warnbe);
 wmem.ban();
 }
 
 message.channel.send(`${message.member.displayName} has banned ${wmem.displayName}`).then(msg => {msg.delete(5000)});
-console.log(`${message.member.displayName} has banned ${wmem.displayName} from the server ${message.guild}`);
-
+message.delete(5000);
 
 
 }
