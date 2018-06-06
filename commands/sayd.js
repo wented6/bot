f4a87b08396fb3035641002d3806b9e0704545b8
nekoboy7409 <38514326+nekoboy7409@users.noreply.github.com>
2018-06-06 04:41:24 -0400

    Update sayd.js

--- commands/sayd.js
+++ commands/sayd.js
@@ -1,9 +1,17 @@
 const Discord = require("discord.js");
 
 module.exports.run = async (client,message,args) => {
+  let mem = message.mentions.members.first();
 
   message.delete();
   let botmessage = args.join(" ");
+  
+  if(botmessage.includes("@here")) botmessage = botmessage.repalce("@here", "@ here");
+  if(botmessage.includes("@everyone")) botmessage = botmessage.repalce("@everyone", "@ everyone");
+  if(botmessage.includes(mem)) botmessage = botmessage.repalce(mem, `@ ${mem.displayName}`);
+  
+
+
   message.channel.send(botmessage);
 }
 
