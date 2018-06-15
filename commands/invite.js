1d299788cbc83e5bbdc5b6481dd744528f831d71
nekoboy7409 <38514326+nekoboy7409@users.noreply.github.com>
2018-06-15 03:47:29 -0400

    Update invite.js

--- commands/invite.js
+++ commands/invite.js
@@ -7,9 +7,9 @@ module.exports.run = async (client,message,args) => {
 	.setTimestamp()
 	.setThumbnail(`${client.user.avatarURL}`)
 	.setTitle(`links:`)
-	.addField("**link to invite me to a server:**",`[Nekobot™/invite](https://discordapp.com/oauth2/authorize?client_id=${client.user.id}&scope=bot&permissions=8)`)
-	.addField("**link to my support sever:**", `[Nekobot™ official](https://discord.gg/SQFbf9q)`)
-	.addField("**if you like the bot, show it:**", `[upvote](https://discordbots.org/bot/389890733576028161/vote)`)
+	.addField("**links to invite me to a server:**",`[Nekobot™/invite (admin)](https://discordapp.com/oauth2/authorize?client_id=${client.user.id}&scope=bot&permissions=8) || [Nekobot™/invite (mod)](https://discordapp.com/oauth2/authorize?client_id=${client.user.id}&scope=bot&permissions=1412820055)`)
+	.addField("**links to my support severs:**", `[Nekobot™ official](https://discord.gg/SQFbf9q) || [Jiko's bots](https://discord.gg/4eMsYmx)`)
+	.addField("**bot list links:**", `[Discord_Bot_List/Nekobot™](https://discordbots.org/bot/389890733576028161) || [Discord_Bots/Nekobot™](https://bots.discord.pw/bots/389890733576028161) || [Bots_for_Discord/Nekobot™](https://botsfordiscord.com/bot/389890733576028161) || [botlist.space/Nekobot™](https://botlist.space/view/389890733576028161)`)
 	message.channel.send({embed: embed}).then(message => {message.delete(21000)});
 console.log(`sent links to ${message.author.username} in ${message.guild}`);
 	message.delete(20000);
