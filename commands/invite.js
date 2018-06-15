bf07c1cbd59a0adaf7a04824bcab3e5a0fffb205
nekoboy7409 <38514326+nekoboy7409@users.noreply.github.com>
2018-06-15 04:03:01 -0400

    Update invite.js

--- commands/invite.js
+++ commands/invite.js
@@ -9,7 +9,7 @@ module.exports.run = async (client,message,args) => {
 	.setTitle(`links:`)
 	.addField("**link to invite me to a server:**",`[Nekobot™/invite (admin)](https://discordapp.com/oauth2/authorize?client_id=${client.user.id}&scope=bot&permissions=8) || [Nekobot™/invite (mod)](https://discordapp.com/oauth2/authorize?client_id=${client.user.id}&scope=bot&permissions=1412820055)`)
 	.addField("**link to my support sever(s):**", `[Nekobot™ official](https://discord.gg/SQFbf9q) || [Jiko's bots](https://discord.gg/4eMsYmx)`)
-	.addField("**bot list links:**", `[Discord_Bot_List/Nekobot™](https://discordbots.org/bot/389890733576028161) \n [Discord_Bots/Nekobot™](https://bots.discord.pw/bots/389890733576028161) \n [Bots_for_Discord/Nekobot™](https://botsfordiscord.com/bot/389890733576028161) \n [botlist.space/Nekobot™](https://botlist.space/view/389890733576028161)`)
+	.addField("**bot list links:**", `[Discord_Bot_List/Nekobot™](https://discordbots.org/bot/389890733576028161) \n[Discord_Bots/Nekobot™](https://bots.discord.pw/bots/389890733576028161) \n[Bots_for_Discord/Nekobot™](https://botsfordiscord.com/bot/389890733576028161) \n[botlist.space/Nekobot™](https://botlist.space/view/389890733576028161)`)
 	message.channel.send({embed: embed}).then(message => {message.delete(120000)});
 console.log(`sent links to ${message.author.username} in ${message.guild}`);
 	message.delete(20000);
