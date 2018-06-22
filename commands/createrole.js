18638d2a3ae115d90835e50c49ae2680c8b2bf38
nekoboy7409 <38514326+nekoboy7409@users.noreply.github.com>
2018-06-22 03:54:47 -0400

    Add files via upload

---
+++ commands/createrole.js
@@ -0,0 +1,22 @@
+const Discord = require("discord.js");
+
+module.exports.run = async (client,message,args) => {
+	
+let name = message.content.split(" ").slice(1, 2).join(" ");
+let color = message.content.split(" ").slice(2).join(" ");
+let rle = message.guild.roles.find("name", name);
+if(rle)return message.channel.send("sorry, but that's already a role in this guild");
+let sum1 = message.member
+    if (!sum1.hasPermission("MANAGE_ROLES") && message.author.id !== "377271843502948354")return message.channel.send('you need to have the ``"MANAGE_ROLES"`` permission to use this command');
+	if (sum1.hasPermission("MANAGE_ROLES")) {
+	message.guild.createRole({
+		name: name,
+		color: color
+	});
+  }
+	message.channel.send(`created role ${name}`);
+	
+}
+module.exports.help = {
+    name: "createrole"
+}
