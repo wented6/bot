11fa63edc3729aa6ef4c66c2c561e2bdc9f27976
nekoboy7409 <38514326+nekoboy7409@users.noreply.github.com>
2018-06-23 05:00:10 -0400

    Update serverinfo.js

--- commands/serverinfo.js
+++ commands/serverinfo.js
@@ -4,11 +4,14 @@ module.exports.run = async (client,message,args) => {
 	
 let ppl = message.guild.members.filter(mem => mem.user.bot === false).size;
 let bots = message.guild.members.filter(mem => mem.user.bot === true).size;
+let pplonl = message.guild.members.filter(mem => mem.user.bot === false && mem.presence.status !== "offline").size;
+let botsonl = message.guild.members.filter(mem => mem.user.bot === true && mem.presence.status !== "offline").size;
+let pplofl = message.guild.members.filter(mem => mem.user.bot === false && mem.presence.status == "offline").size;
+let botsofl = message.guild.members.filter(mem => mem.user.bot === true && mem.presence.status == "offline").size;
 
 let txt = message.guild.channels.filter(c => c.type === 'text').size;
 let vcs = message.guild.channels.filter(c => c.type === 'voice').size;
 let cats = message.guild.channels.filter(c => c.type === 'category').size;
-let onr = message.guild.owner;
 
 if(onr){
 const RichEmbed = new Discord.RichEmbed()
@@ -16,15 +19,14 @@ const RichEmbed = new Discord.RichEmbed()
        .setTimestamp()
 	   .setThumbnail(message.guild.iconURL)
 	   .setTitle(`${message.guild}`)
-	   .addField('**Owner:**', `${onr.displayName}`)
+	   .addField('**Owner:**', `${message.guild.owner.displayName}`)
        .addField('**Region:**', `${message.guild.region}`)
        .addField('**# of Roles:**', `${message.guild.roles.size}`)
-	   .addField('**# of Members:**', `${ppl} members, ${bots} bots`)
+	   .addField('**Members:**', `\`\`\`${ppl} users - ${pplonl} online | ${pplofl} offline\n${bots} bots - ${botsonl} online | ${botsofl} offline\n${message.guild.members.size} total\`\`\``)
 	   .addField('**# of Emojis:**', `${message.guild.emojis.size}`)
-       .addField('**Server Created:**', `${message.guild.createdAt}`)
-	   .addField('**(cats/txt/vc):**', `${cats}/ ${txt}/ ${vcs}`)
-	   message.channel.send({embed: RichEmbed})
-	   console.log(`serverinfo command has been used by ${message.author.username} in ${message.guild}`);
+       .addField('**Server Created:**', `${message.guild.createdAt.toUTCString()}`)
+	   .addField('**Channels:**', `${cats} categories\n${txt} text\n${vcs} voice`)
+	   message.channel.send({embed: RichEmbed});
 }
 if(!onr){
 const RichEmbed = new Discord.RichEmbed()
@@ -32,14 +34,14 @@ const RichEmbed = new Discord.RichEmbed()
        .setTimestamp()
 	   .setThumbnail(message.guild.iconURL)
 	   .setTitle(`${message.guild}`)
+	   .addField('**Owner:**', `none`)
        .addField('**Region:**', `${message.guild.region}`)
        .addField('**# of Roles:**', `${message.guild.roles.size}`)
-	   .addField('**# of Members:**', `${ppl} members, ${bots} bots`)
+	   .addField('**Members:**', `\`\`\`${ppl} users - ${pplonl} online | ${pplofl} offline\n${bots} bots - ${botsonl} online | ${botsofl} offline\n${message.guild.members.size} total\`\`\``)
 	   .addField('**# of Emojis:**', `${message.guild.emojis.size}`)
-       .addField('**Server Created:**', `${message.guild.createdAt}`)
-	   .addField('**(cats/txt/vc):**', `${cats}/ ${txt}/ ${vcs}`)
-	   message.channel.send({embed: RichEmbed})
-	   console.log(`serverinfo command has been used by ${message.author.username} in ${message.guild}`);
+       .addField('**Server Created:**', `${message.guild.createdAt.toUTCString()}`)
+	   .addField('**Channels:**', `${cats} categories\n${txt} text\n${vcs} voice`)
+	   message.channel.send({embed: RichEmbed});
 }
 
 }
